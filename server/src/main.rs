3b63ce9cfe6d77c4d182220d17040cb4752997e3
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-09-25 01:38:13 +0200

    feat(server): use tracing for logging

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,13 +4,19 @@ mod utils;
 
 use crate::chat_service::ChatService;
 use proto::chat::chat_server::ChatServer;
+use tracing::{info, Level};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .compact()
+        .with_max_level(Level::INFO)
+        .init();
+
     let addr = "[::1]:50051".parse()?;
     let chat_service = ChatService::default();
 
-    println!("Server listening on {}", addr);
+    info!("Server listening on {}", addr);
 
     tonic::transport::Server::builder()
         .add_service(ChatServer::new(chat_service))
